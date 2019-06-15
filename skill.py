e5376f002e865045c116903a811a48ef722d6b95
Ryan-Gadhi <ryangadhi@yahoo.com>
2019-06-15 23:13:48 +0300

    added skills to interact with the system

---
+++ skill.py
@@ -0,0 +1,23 @@
+import pyttsx
+import random
+
+
+class Skill:
+
+	def __init__(self, one_input, one_output):
+		self.input_choices = []
+		self.output_choices = []
+
+		for choice in one_input:
+			self.input_choices.append(choice)
+
+		for choice in one_output:
+			self.output_choices.append(choice)
+
+	def reply(self):
+		engine = pyttsx.init()
+
+		random_reply = random.choice(self.output_choices)
+		engine.say(random_reply)
+		engine.runAndWait()
+
---
+++ speaking.py
@@ -0,0 +1,5 @@
+import pyttsx
+
+engine = pyttsx.init()
+engine.say('Good morning, what the hell! are you doing! over here!')
+engine.runAndWait()
